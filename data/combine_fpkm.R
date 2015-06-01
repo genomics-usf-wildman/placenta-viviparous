d21e0987d525ca4c51c8e77b2d178148c38f67f1
Don Armstrong <don@donarmstrong.com>
2015-06-01 12:33:00 -0700

    use the updated human fpkm file

--- data/combine_fpkm.R
+++ data/combine_fpkm.R
@@ -11,7 +11,7 @@ homology.file <- args[2]
 reads <- list()
 for (fpkm.file in fpkm.files) {
     reads[[fpkm.file]] <- fread(fpkm.file)
-    reads[[fpkm.file]][,species:=gsub("^.+Table_\\d+_(.+?)_(?:[Gg]enes|From).+$","\\1",fpkm.file)]
+    reads[[fpkm.file]][,species:=gsub("^.+Table_\\d+_(.+?)_(?:[Gg]enes|From|NEW).+$","\\1",fpkm.file)]
     ## these files have some things misnamed or differently named
     rename.cols <- list("status"="FPKM_status",
                         "HUMAN NAME"="Human Name",
@@ -29,7 +29,7 @@ for (fpkm.file in fpkm.files) {
     }
 }
 
-### from ftp://ftp.informatics.jax.org/pub/reports/HOM_MouseHumanSequence.rpt
+### from ftp://ftp.informatics.jax.org/pub/reports/HOM_AllOrganism.rpt
 homology.mouse.human <- fread(homology.file)
 setnames(homology.mouse.human,"HomoloGene ID","homologene.id")
 setnames(homology.mouse.human,"Common Organism Name","organism.name")
@@ -37,14 +37,26 @@ homology.table <-
     data.table(dcast(homology.mouse.human,
                      homologene.id~organism.name,
                      fun.aggregate=function(x){x[1]},value.var="Symbol"))
-setnames(homology.table,"mouse, laboratory","mouse")
+setnames(homology.table,
+         c("mouse, laboratory",
+           "dog, domestic",
+           "rhesus macaque",
+           "western clawed frog"
+           ),
+         c("mouse",
+           "dog",
+           "macaque",
+           "frog"
+           ))
 setkey(homology.table,"mouse")
 mouse.human.homolog <- function(mouse.gene) {
     return(homology.table[mouse==mouse.gene,human])
 }
 
 combined.fpkm <- rbindlist(reads,fill=TRUE)
+combined.fpkm[gene_short_name=="GH1 (CSHL1)",gene_short_name:="CSHL1"]
 combined.fpkm[species=="Mouse","Human Name":=sapply(gene_short_name,mouse.human.homolog)]
+combined.fpkm[species=="Homo","Human Name":=gene_short_name]
 combined.fpkm[,V15:=NULL]
 combined.fpkm[,V14:=NULL]
 

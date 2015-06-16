f66b86980829a4675764bd1b2e6b43c288c6f352
Don Armstrong <don@donarmstrong.com>
2015-06-16 11:07:13 -0700

    add rules for homology table; use fpkm files

--- data/combine_fpkm.R
+++ data/combine_fpkm.R
@@ -1,33 +1,19 @@
 library(data.table)
 library(reshape2)
+library(parallel)
+
 args <- commandArgs(trailingOnly=TRUE)
 
-fpkm.files <- args[-(1:3)]
+if (is.null(getOption("mc.cores"))) {
+    options(mc.cores=8)
+}
 
 output.file <- args[1]
 
 homology.file <- args[2]
 
-reads <- list()
-for (fpkm.file in fpkm.files) {
-    reads[[fpkm.file]] <- fread(fpkm.file)
-    reads[[fpkm.file]][,species:=gsub("^.+Table_\\d+_(.+?)_(?:[Gg]enes|From|NEW).+$","\\1",fpkm.file)]
-    ## these files have some things misnamed or differently named
-    rename.cols <- list("status"="FPKM_status",
-                        "HUMAN NAME"="Human Name",
-                        "Gene Name"="Human Name",
-                        "Human ID"="Human Name",
-                        "Human Homolog"="Human Name"
-                        )
-    for (col.name in names(rename.cols)) {
-        if (any(col.name %in% colnames(reads[[fpkm.file]])))  {
-            setnames(reads[[fpkm.file]],col.name,rename.cols[[col.name]])
-        }
-    }
-    if (any("other comments" %in% colnames(reads[[fpkm.file]]))) {
-        reads[[fpkm.file]][,"other comments":=NULL]
-    }
-}
+load(args[4])
+
 
 ### from ftp://ftp.informatics.jax.org/pub/reports/HOM_AllOrganism.rpt
 homology.mouse.human <- fread(homology.file)
@@ -53,54 +39,41 @@ human.homolog <- function(gene,species="mouse") {
                           human][1])
 }
 
-combined.fpkm <- rbindlist(reads,fill=TRUE)
-## CSHL1 is not the same as GH1; call GH1 in Pan an ortholog of CSHL1
-setnames(combined.fpkm,
-         "Human Name",
-         "human_name")
-
 oma.groups <- fread(args[3])
 oma.groups[,species:=gsub("\\d+","",oma_entry)]
 setkey(oma.groups,"ensembl")
 oma.groups.human <- oma.groups[species=="HUMAN"]
 
 oma.homolog <- function(ensembl.id){
-    combined.fpkm[tracking_id %in% oma.groups.human[group_num==oma.groups[ensembl.id,group_num],
+    gene.fpkms[tracking_id %in% oma.groups.human[group_num==oma.groups[ensembl.id,group_num],
                                                     ensembl],
                   gene_short_name][1]
 }
 
-combined.fpkm[gene_short_name=="GH1 (CSHL1)",gene_short_name:="CSHL1"]
-combined.fpkm[gene_short_name=="Q95MK7_PANTR",human_name:="CSHL1"]
-combined.fpkm[species=="Homo",human_name:=gene_short_name]
-combined.fpkm[human_name=="",human_name:=NA]
-combined.fpkm[human_name=="-",human_name:=NA]
-combined.fpkm[grepl(" ",human_name),
+gene.fpkms[species=="homo sapiens",human_name:=gene_short_name]
+gene.fpkms[human_name=="",human_name:=NA]
+gene.fpkms[human_name=="-",human_name:=NA]
+gene.fpkms[grepl(" ",human_name),
               human_name:=NA]
 ### Mouse
-combined.fpkm[species=="Mouse" & is.na(human_name),
-              human_name := sapply(gene_short_name,function(x){human.homolog(x,species="mouse")})]
+gene.fpkms[species=="mus musculus" & is.na(human_name),
+           human_name := mcmapply(function(x){human.homolog(x,species="mouse")},gene_short_name)]
 ### Canis (dog)
-combined.fpkm[species=="Canis" & is.na(human_name),
-              human_name := sapply(gene_short_name,function(x){human.homolog(x,species="dog")})]
+gene.fpkms[species=="canis familiaris" & is.na(human_name),
+              human_name := mcmapply(function(x){human.homolog(x,species="dog")},gene_short_name)]
 ### Cow (cattle)
-combined.fpkm[species=="Cow" & is.na(human_name),
-              human_name := sapply(gene_short_name,function(x){human.homolog(x,species="cattle")})]
-### Pan
-combined.fpkm[species=="Pan" & is.na(human_name),
-              human_name := sapply(gene_short_name,function(x){human.homolog(x,species="chimpanzee")})]
+gene.fpkms[species=="bos taurus" & is.na(human_name),
+              human_name := mcmapply(function(x){human.homolog(x,species="cattle")},gene_short_name)]
+### pan paniscus
+gene.fpkms[species=="pan paniscus" & is.na(human_name),
+              human_name := mcmapply(function(x){human.homolog(x,species="chimpanzee")},gene_short_name)]
 
-combined.fpkm[is.na(human_name),
-              human_name := sapply(gene_id,oma.homolog)]
-
-combined.fpkm[,V15:=NULL]
-combined.fpkm[,V14:=NULL]
-
-
+gene.fpkms[is.na(human_name),
+              human_name := mcmapply(oma.homolog,gene_id)]
 
 ### this shouldn't be required, but I've triggered some bug in the
 ### data.table code
-combined.fpkm <- data.table(data.frame(combined.fpkm))
+combined.fpkm <- data.table(data.frame(gene.fpkms))
 
 save(file=output.file,combined.fpkm)
 
---
+++ data/homology_table.R
@@ -0,0 +1,26 @@
+
+library(reshape2)
+library(data.table)
+
+args <- commandArgs(trailingOnly=TRUE)
+
+### combined_fpkm
+load(args[1])
+
+homology.table <-
+    dcast(combined.fpkm,
+          human_name~species,
+          value.var="mean_fpkm",
+          fun.aggregate=function(x){x[1]})
+
+### ditch rows which are all zero
+
+gene.sum <-
+    apply(homology.table,1,function(x){sum(as.numeric(x[2:9]),na.rm=TRUE)})
+homology.table <- homology.table[gene.sum>0 & !is.na(homology.table$human_name),]
+rownames(homology.table) <-
+    homology.table[,1]
+homology.table <- data.matrix(homology.table[,-1])
+
+save(homology.table,
+     file=args[length(args)])

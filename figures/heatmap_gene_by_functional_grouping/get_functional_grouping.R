0d8868b8135b06a2aee8050c2d8da95fc7ae5ad2
Don Armstrong <don@donarmstrong.com>
2015-06-24 09:14:21 -0700

    fix how the homo sapiens tracking ids are retrieved

--- figures/heatmap_gene_by_functional_grouping/get_functional_grouping.R
+++ figures/heatmap_gene_by_functional_grouping/get_functional_grouping.R
@@ -13,7 +13,7 @@ mart <- useMart(biomart="ensembl",dataset="hsapiens_gene_ensembl")
 results <- getBM(attributes=c("ensembl_gene_id","hgnc_symbol",
                      "go_id","name_1006","go_linkage_type","namespace_1003"),
                  filters="ensembl_gene_id",
-                 values=combined.fpkm[species=="Homo",tracking_id],
+                 values=combined.fpkm[species=="homo sapiens",tracking_id],
                  mart=mart)
 go.functional.groups <- data.table(results)
 

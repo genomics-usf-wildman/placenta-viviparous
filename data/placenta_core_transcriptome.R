5cbad562640816ec32201dfc9fb7eef0d1df556f
Don Armstrong <don@donarmstrong.com>
2015-10-16 14:40:41 -0500

    show the shape of the core placenta transcriptome

--- data/placenta_core_transcriptome.R
+++ data/placenta_core_transcriptome.R
@@ -15,11 +15,39 @@ combined.fpkm.wide <-
                      fun.aggregate=sum,
                      value.var="mean_fpkm"))
 
+min.na.zero <- function(x){
+    x[is.na(x)] <- 0
+    return(min(x))
+}
+
+max.na.zero <- function(x){
+    return(max(x,na.rm=TRUE))
+}
+
+min.but.one <- function(x,n=1){
+    x[is.na(x)] <- 0
+    while (n > 0) {
+        x <- x[-which.min(x)]
+        n <- n - 1
+    }
+    return(min(x))
+}
+
 combined.fpkm.wide[,median_expression :=
                        apply(combined.fpkm.wide[,-1,with=FALSE],
                              1,median)]
+combined.fpkm.wide[,max_expression :=
+                       apply(combined.fpkm.wide[,-1,with=FALSE],1,max.na.zero)]
+combined.fpkm.wide[,fifth_min_expression :=
+                       apply(combined.fpkm.wide[,-1,with=FALSE],1,function(x){min.but.one(x,4)})]
+combined.fpkm.wide[,fourth_min_expression :=
+                       apply(combined.fpkm.wide[,-1,with=FALSE],1,function(x){min.but.one(x,3)})]
+combined.fpkm.wide[,third_min_expression :=
+                       apply(combined.fpkm.wide[,-1,with=FALSE],1,function(x){min.but.one(x,2)})]
+combined.fpkm.wide[,second_min_expression :=
+                       apply(combined.fpkm.wide[,-1,with=FALSE],1,min.but.one)]
 combined.fpkm.wide[,min_expression :=
-                       apply(combined.fpkm.wide[,-1,with=FALSE],1,min)]
+                       apply(combined.fpkm.wide[,-1,with=FALSE],1,min.na.zero)]
 setkey(combined.fpkm.wide,"human_name")
 
 ## all genes which are not housekeeping genes which have minimum 
@@ -37,10 +65,19 @@ core.placenta.transcriptome.long[,human_name:=
                                              combined.fpkm.wide[core.placenta.transcriptome.long[,human_name],median_expression])]
 
 
+core.placenta.transcriptome.genes.shape <-
+    combined.fpkm.wide[,list(human_name,
+                             median_expression,
+                             min_expression,
+                             second_min_expression,
+                             third_min_expression,
+                             fourth_min_expression,
+                             fifth_min_expression,
+                             max_expression)]
+
 all.genes <- combined.fpkm.wide[!is.na(human_name),unique(human_name)]
 placenta.transcriptome.genes <- 
     core.placenta.transcriptome.long[!is.na(human_name),unique(human_name)]
-
 
 pts <- data.table(all.genes=all.genes)
 pts <- pts[,placenta.ts:=all.genes %in% placenta.transcriptome.genes]
@@ -57,6 +94,7 @@ placenta.transcriptome.list <-
 
 save(core.placenta.transcriptome.long,
      placenta.transcriptome.list,
+     core.placenta.transcriptome.genes.shape,
      pts,
      file=args[length(args)])
 

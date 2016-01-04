9fc6c437f156775cacbefdeeeb23b3977329a7f5
Don Armstrong <don@donarmstrong.com>
2016-01-04 15:57:29 -0800

    fix tree
    fix spacing of gene names
    vertical species names
    ggplot default gradient

--- common_r_code/plot_tree_matrix.R
+++ common_r_code/plot_tree_matrix.R
@@ -1,21 +1,25 @@
-plot.tree.matrix <- function(gene.tree,gene.tree.table,expression,min.fpkm=NA) {
+library("magrittr")
+library("tidyr")
+plot.tree.matrix <- function(gene.tree,gene.tree.table,gene.tree.expression,offset.ratio=0.5,min.fpkm=NA) {
     gene.tree$tip.label <- gsub("^(ENS.+)\\1$","\\1",gene.tree$tip.label)
     gene.tree.table[,species:=capfirst(gsub("_"," ",species))]
-    gene.tree.table[,symbol_or_id:=ifelse(symbol=="NULL",gene_id,paste0(symbol," ",species))]
+    gene.tree.table[,short.species:=gsub("^(.)[^ ]* +([^ ]*)","\\1. \\2",species)]
+    gene.tree.table[,symbol_or_id:=ifelse(symbol=="NULL",gene_id,paste0(symbol," ",short.species))]
     gene.tree.table[duplicated(symbol_or_id),symbol_or_id:=paste0(symbol_or_id," dup")]
-    gene.tree.expression <-
-        combined.fpkm[gene_id %in% gene.tree.table[,gene_id]]
+    gene.tree.expression.subset <- gene.tree.expression
     if (!is.na(min.fpkm)) {
-        gene.tree.expression <-
+        gene.tree.expression.subset <-
             gene.tree.expression[mean_fpkm >= min.fpkm ,]
     }
     setkey(gene.tree.table,"protein_id")
     gene.tree$tip.label <-
         gene.tree.table[gene.tree$tip.label,gene_id]
     gene.tree.subset <-
-        drop.tip(gene.tree,gene.tree$tip.label[!(gene.tree$tip.label %in%
-                                                     gene.tree.expression[,gene_id])])
-
+        drop.tip(gene.tree,
+                 gene.tree$tip.label[!(gene.tree$tip.label %in%
+                                       gene.tree.expression.subset[,gene_id])])
+    gene.tree.expression <-
+        gene.tree.expression[gene_id %in% gene.tree$tip.label,]
     gene.tree.expression[,mean_fpkm_log:=log10(mean_fpkm+1)]
     gene.tree.expression.matrix <-
         dcast(gene.tree.expression,
@@ -29,7 +33,7 @@ plot.tree.matrix <- function(gene.tree,gene.tree.table,expression,min.fpkm=NA) {
     colnames(gene.tree.expression.matrix) <-
         gsub("(^)(.)[^ ]*","\\2.",colnames(gene.tree.expression.matrix))
     colnames(gene.tree.expression.matrix) <-
-        gsub("( )(..)[^ ]*","\\2.",colnames(gene.tree.expression.matrix))
+        gsub("( +)([^ ][^ ])[^ ]* *$","\\2",colnames(gene.tree.expression.matrix))
 
 
     setkey(gene.tree.table,"gene_id")
@@ -40,12 +44,86 @@ plot.tree.matrix <- function(gene.tree,gene.tree.table,expression,min.fpkm=NA) {
     gene.tree.subset$tip.label <-
         gene.tree.table[gene.tree.subset$tip.label,symbol_or_id]
     p <- ggtree(gene.tree.subset)# %>% add_legend(x=2008, y=5)
-    p <- p + geom_tiplab(size=3,align=TRUE)
+    p <- p + geom_tiplab(aes(label=""),align=TRUE,size=3)
     
-    
-    print(gheatmap(p,data=gene.tree.expression.matrix,offset=0.3,colnames=TRUE) +
-              theme(legend.title=element_text())
-          + scale_fill_gradient(low="white",high="green",na.value="gray50",
+
+    text.labels <- p$data[!is.na(p$data$label),]
+    text.labels$xpos <- max(p$data$x)+max(p$data$x)*offset.ratio/2
+    print(don.gheatmap(p,data=gene.tree.expression.matrix,offset=max(p$data$x)*offset.ratio,colnames=TRUE)
+          + geom_text(aes(x=xpos,
+                          y=y,label=label,
+                          hjust=0.5),
+                      data=text.labels
+                      )
+          + theme(legend.title=element_text())
+          + scale_fill_gradient(low = "#132B43", high = "#56B1F7", space = "Lab", na.value = "grey50",
                                 name=expression(log[10](FPKM)))
           )
+
+}
+
+don.gheatmap <- function(p, data, offset=0, width=1, low="green", high="red",
+                     color="white", colnames=TRUE, colnames_position="bottom", font.size=4) {
+
+    colnames_position %<>% match.arg(c("bottom", "top"))
+    variable <- value <- lab <- y <- NULL
+    
+    ## if (is.null(width)) {
+    ##     width <- (p$data$x %>% range %>% diff)/30
+    ## }
+
+    ## convert width to width of each cell
+    width <- width * (p$data$x %>% range %>% diff) / ncol(data)
+    
+    isTip <- x <- y <- variable <- value <- from <- to <- NULL
+ 
+    df=p$data
+    df=df[df$isTip,]
+    start <- max(df$x) + offset
+
+    dd <- data[df$label[order(df$y)],]
+    dd$y <- sort(df$y)
+
+    dd$lab <- rownames(dd)
+    ## dd <- melt(dd, id=c("lab", "y"))
+    dd <- gather(dd, variable, value, -c(lab, y))
+    
+    dd$value[is.na(dd$value) | dd$value == ""] <- NA
+
+    V2 <- start + as.numeric(dd$variable) * width
+    mapping <- data.frame(from=dd$variable, to=V2)
+    mapping <- unique(mapping)
+
+    dd$x <- V2
+
+    if (is.null(color)) {
+        p2 <- p + geom_tile(data=dd, aes(x, y, fill=value), inherit.aes=FALSE)
+    } else {
+        p2 <- p + geom_tile(data=dd, aes(x, y, fill=value), color=color, inherit.aes=FALSE)
+    }
+    if (is(dd$value,"numeric")) {
+        p2 <- p2 + scale_fill_gradient(low=low, high=high, na.value="white")
+    } else {
+        p2 <- p2 + scale_fill_discrete(na.value="white")
+    }
+    
+    if (colnames) {
+        if (colnames_position == "bottom") {
+            y <- min(p$data$y)-0.6
+        } else {
+            y <- max(p$data$y) + 1
+        }
+        p2 <- p2 + geom_text(data=mapping,
+                             aes(x=to, label=from),
+                             angle=90,
+                             hjust=1,
+                             vjust=0.5,
+                             y=y, size=font.size, inherit.aes = FALSE)
+    }
+
+    p2 <- p2 + theme(legend.position="right", legend.title=element_blank())
+    p2 <- p2 + guides(fill = guide_legend(override.aes = list(colour = NULL)))
+    
+    attr(p2, "mapping") <- mapping
+    return(p2)
 }

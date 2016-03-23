919f90200e1d2fe354afcb81b9e98b9a193a7a4a
Don Armstrong <don@donarmstrong.com>
2016-03-23 15:53:07 -0700

    use parallel in human mouse lineage specific

--- data/human_mouse_lineage_specific.R
+++ data/human_mouse_lineage_specific.R
@@ -1,4 +1,5 @@
 library("data.table")
+library("parallel")
 library("reshape2")
 library("org.Hs.eg.db")
 library("lme4")
@@ -25,7 +26,7 @@ placenta.lineage.specific <-
 setkey(combined.fpkm,"human_name")
 
 calculate.mann.whitney.test <- function(x,g1,g2){
-    data.subset <- combined.fpkm[x,list(species,FPKM)]
+    data.subset <- combined.fpkm[x,list(species,FPKM=sum(FPKM)),by=file][!duplicated(file),]
     data.subset[,this.group:=NA]
     data.subset[species %in% g1,this.group:=TRUE]
     data.subset[species %in% g2,this.group:=FALSE]
@@ -35,6 +36,8 @@ calculate.mann.whitney.test <- function(x,g1,g2){
         this.sum <-
             summary(lmer(FPKM~1+this.group+(1|species),
                          data.subset))
+        ## this will actually underestimate p, but that's OK, because
+        ## we have a pretty large n here anyway
         p.value <- pt(this.sum$coefficients["this.groupTRUE",
                                             "t value"],
                       df=this.sum$ngrps,lower.tail=FALSE)
@@ -49,10 +52,10 @@ calculate.fc <- function(x,g1,g2){
     try({
         first <- combined.fpkm[x][species %in% g1,FPKM]
         second <- combined.fpkm[x][species %in% g2,FPKM]
-        fc <- mean(as.numeric(first))/mean(as.numeric(second))
-        if (fc < 1) {fc <- -1/fc}
+        fc <- log2(mean(as.numeric(first))/mean(as.numeric(second)))
+        ## if (fc < 1) {fc <- -1/fc}
         if (any(!is.finite(fc))) {
-            fc <- max(c(mean(as.numeric(first)),mean(as.numeric(second)),na.rm=TRUE))
+            fc <- log2(max(c(mean(as.numeric(first)),mean(as.numeric(second)),na.rm=TRUE)))
         }},
         silent=TRUE)
     return(fc)
@@ -74,19 +77,21 @@ human.mouse.lineage.specific.analysis <- list()
 for (c.n in names(comparisons)) {
     results <- data.table(placenta.lineage.specific[,human_name])
     setnames(results,"human_name")
-    results[,p.value:=apply(placenta.lineage.specific,1,
-                            calculate.mann.whitney.test,
-                            comparisons[[c.n]]$group1,
-                            comparisons[[c.n]]$group2
-                            )]
+    results[,p.value:=unlist(mclapply(placenta.lineage.specific[,human_name],
+                                      calculate.mann.whitney.test,
+                                      comparisons[[c.n]]$group1,
+                                      comparisons[[c.n]]$group2,
+                                      mc.cores=4
+                                      ))]
     results[,fdr:=p.adjust(results[,p.value],method="BH")]
     results[,group1.name:=comparisons[[c.n]]$group1.name]
     results[,group2.name:=comparisons[[c.n]]$group2.name]
-    results[,fc:=apply(placenta.lineage.specific,1,
-                       calculate.fc,
-                       comparisons[[c.n]]$group1,
-                       comparisons[[c.n]]$group2
-                       )
+    results[,fc:=unlist(mclapply(placenta.lineage.specific[,human_name],
+                                 calculate.fc,
+                                 comparisons[[c.n]]$group1,
+                                 comparisons[[c.n]]$group2,
+                                 mc.cores=4
+                                 ))
             ]
     results[,fdr:=p.adjust(results[,p.value],method="BH")]
     results[,analysis.name:=c.n]

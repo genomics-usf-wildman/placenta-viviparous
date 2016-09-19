3350d1a203bbf14330460f4be311721d706fd9ba
Don Armstrong <don@donarmstrong.com>
2016-09-18 17:42:29 -0700

    build placenta classification

---
+++ data/placenta_classification.R
@@ -0,0 +1,32 @@
+library("data.table")
+
+
+args <- c("../data/placental_classification.txt","placental_classification")
+args <- commandArgs(trailingOnly=TRUE)
+
+placenta_classification <- fread(args[1])
+
+placenta_classification[,species:=factor(species)]
+placenta_classification[,shape:=factor(shape)]
+placenta_classification[,interdigitiation:=
+                             factor(interdigitation,
+                                    levels=c("villous",
+                                             "folded",
+                                             "trabecular",
+                                             "lamellar",
+                                             "labrynthine"
+                                             ),
+                                    ordered=TRUE
+                                    )]
+
+placenta_classification[,barrier:=
+                             factor(barrier,
+                                    levels=c("epitheliochorial",
+                                             "endotheliochorial",
+                                             "hemochorial"
+                                             ),
+                                    ordered=TRUE
+                                    )]
+
+save(placenta_classification,
+     file=args[length(args)])

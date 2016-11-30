50e2f02b38650dba4909072ebc56726307aeb79a
Don Armstrong <don@donarmstrong.com>
2016-11-30 13:48:46 -0800

    use short/single/long options in acro

--- manuscript/gene_names.sh
+++ manuscript/gene_names.sh
@@ -2,5 +2,5 @@
 
 for gene in "$@"; do
     name="$(echo "$gene" | gene_to_aliases|awk -F"$(echo -ne "\t")" '{print $2}')"
-    echo "\\DeclareAcronym{$gene}{short=\\textit{$gene},long={$name}}";
+    echo "\\DeclareAcronym{$gene}{short=\\textit{$gene},long={$name},first-style=reversed,single={\\textit{$gene} ($name)}}";
 done;

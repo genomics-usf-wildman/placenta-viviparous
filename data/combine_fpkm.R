ae1d74301003b87adb40fc7562577a3b47b1ba23
Don Armstrong <don@donarmstrong.com>
2017-03-08 09:51:47 -0800

    Switch to using OMA to define ortholog pairs
     - Use ensembl as a backup
     - Test that the orthologs are correctly assigned
     - Completely codify the rules in source and check them

--- data/combine_fpkm.R
+++ data/combine_fpkm.R
@@ -1,6 +1,6 @@
-library(data.table)
-library(reshape2)
-library(parallel)
+library("data.table")
+library("reshape2")
+library("parallel")
 
 args <- commandArgs(trailingOnly=TRUE)
 
@@ -10,59 +10,98 @@ if (is.null(getOption("mc.cores"))) {
 
 output.file <- args[1]
 
-homology.file <- args[2]
+## fastq_fpkm/combined_fpkms
+load(args[3])
 
-load(args[4])
-
-
-### from ftp://ftp.informatics.jax.org/pub/reports/HOM_AllOrganism.rpt
-homology.mouse.human <- fread(homology.file)
-setnames(homology.mouse.human,"HomoloGene ID","homologene.id")
-setnames(homology.mouse.human,"Common Organism Name","organism.name")
-homology.table <-
-    data.table(dcast(homology.mouse.human,
-                     homologene.id~organism.name,
-                     fun.aggregate=function(x){x[1]},value.var="Symbol"))
-setnames(homology.table,
-         c("mouse, laboratory",
-           "dog, domestic",
-           "rhesus macaque",
-           "western clawed frog"
-           ),
-         c("mouse",
-           "dog",
-           "macaque",
-           "frog"
-           ))
-human.homolog <- function(gene,species="mouse") {
-    return(homology.table[eval(parse(text=paste0(species,"==\"",gene,"\""))),
-                          human][1])
-}
-
-oma.groups <- fread(args[3])
+## oma-groups_long.txt
+oma.groups <- fread(args[2])
 oma.groups[,species:=gsub("\\d+","",oma_entry)]
+oma.groups <- oma.groups[grepl("G0",ensembl),]
 setkey(oma.groups,"ensembl")
 oma.groups.human <- oma.groups[species=="HUMAN"]
-setkey(oma.groups.human,"group_num")
+setkey(oma.groups.human,"fingerprint")
 
-oma.homolog <- function(ensembl.id){
-    gene.fpkms[tracking_id %in% oma.groups.human[group_num==oma.groups[ensembl.id,group_num],
-                                                    ensembl],
-                  gene_short_name][1]
+
+### ensembl_all_orthologs
+load(args[4])
+orthologs.subset <-
+    orthologs[secondary=="hsapiens",]
+setkey(orthologs.subset,"ensembl_gene_id")
+
+human_gene_names <-
+    gene.fpkms[species=="homo sapiens",list(gene_short_name=gene_short_name[1]),by=gene_id]
+setkey(human_gene_names,"gene_id")
+
+identify.homolog <- function(ensembl.id) {
+    ## identify the group number from OMA
+    oma.fingerprint <- oma.groups[ensembl.id,fingerprint]
+    ## if there are any human genes with this fingerprint, great, use
+    ## it
+    if (!is.null(oma.fingerprint) &
+        !is.na(oma.fingerprint)) {
+        human.id <- oma.groups.human[oma.fingerprint,
+                                     ensembl]
+        if (!is.null(human.id) && !is.na(human.id)) {
+            homolog <-
+                human_gene_names[human.id,gene_short_name][1]
+            if(!is.null(homolog)) {
+                return(homolog)
+            }
+        }
+    }
+    ## if not, if there is no ensembl ortholog, return NA
+    hum.id <- orthologs.subset[ensembl.id,]
+    if (is.null(hum.id) || is.na(hum.id[,homolog_ensembl_gene_id])) {
+        return(NA)
+    }
+    ## if that ortholog is a 1:1 ortholog, and there isn't another OMA
+    ## group which is that ortholog, use it, otherwise it's the wrong
+    ## ortholog
+    if (hum.id[,all(homolog_orthology_type=="ortholog_one2one")]) {
+        oma.species <- oma.groups[ensembl.id,species]
+        hum.gene.id <- hum.id[,homolog_ensembl_gene_id]
+        human.oma.fingerprint <- oma.groups[hum.gene.id,fingerprint]
+        if (is.na(human.oma.fingerprint) ||
+            length(oma.groups[species==oma.species & fingerprint==human.oma.fingerprint,ensembl])==0 ||
+            is.na(oma.groups[species==oma.species & fingerprint==human.oma.fingerprint,ensembl])) {
+            return(human_gene_names[hum.gene.id,
+                                    gene_short_name[1]])
+        } else {
+            return(NA)
+        }
+    }
+    ## if there is a 1:many ortholog, things are more complicated. If
+    ## there is an existing OMA group with a human member, then return NA.
+    hum.many <-
+        orthologs.subset[homolog_ensembl_gene_id %in% hum.id[,homolog_ensembl_gene_id] &
+                         primary %in% hum.id[,primary],]
+    this.oma.group <-
+        oma.groups[hum.many[,ensembl_gene_id],]
+    if (!all(is.null(oma.groups.human[this.oma.group[!is.na(fingerprint),fingerprint],ensembl])) &&
+        !all(is.na(oma.groups.human[this.oma.group[!is.na(fingerprint),fingerprint],ensembl]))) {
+        return(NA)
+    }
+    ## if no existing OMA group has a human member, but there is only
+    ## one OMA group, that 1:many ortholog is the right one
+    if (this.oma.group[,sum(!is.na(fingerprint))==1] &&
+        this.oma.group[,any(ensembl==ensembl.id & !is.na(fingerprint))]) {
+        hum.gene.id <- hum.id[,homolog_ensembl_gene_id]
+        return(human_gene_names[hum.gene.id,
+                                gene_short_name[1]])
+    }
+    ## otherwise, this is a 1:many ortholog for which we don't know enough about; return NA
+    return(NA)
 }
 
-### ensembl_orthologs
-load(args[5])
-
-orthologs.long <- melt(orthologs,id.vars="ensembl_gene_id")
-orthologs.long[,variable:=NULL]
-setkey(orthologs.long,"value")
-
-ensembl.homolog <- function(ensembl.id) {
-    hum.id <- orthologs.long[ensembl.id,ensembl_gene_id][1]
-    if (is.null(hum.id) || is.na(hum.id))
-        return(NA)
-    return(gene.fpkms[tracking_id==hum.id,gene_short_name][1])
+### double check that the homolog function is working properly
+if (!(identify.homolog("ENSDNOG00000004237")=="ANXA2" &&
+      is.na(identify.homolog("ENSDNOG00000048444")) &&
+      is.na(identify.homolog("ENSBTAG00000000025")) &&
+      identify.homolog("ENSSSCG00000022230")=="CD9" &&
+      is.na(identify.homolog("ENSSSCG00000000710")))
+    ) {
+    print(identify.homolog("ENSDNOG00000004237"))
+    stop("Something is wrong with identify.homolog")
 }
 
 gene.fpkms[species=="homo sapiens",human_name:=gene_short_name]
@@ -70,26 +109,19 @@ gene.fpkms[human_name=="",human_name:=NA]
 gene.fpkms[human_name=="-",human_name:=NA]
 gene.fpkms[grepl(" ",human_name),
               human_name:=NA]
-### ### Mouse
-### gene.fpkms[species=="mus musculus" & is.na(human_name),
-###            human_name := mcmapply(function(x){human.homolog(x,species="mouse")},gene_short_name)]
-### ### Canis (dog)
-### gene.fpkms[species=="canis familiaris" & is.na(human_name),
-###               human_name := mcmapply(function(x){human.homolog(x,species="dog")},gene_short_name)]
-### ### Cow (cattle)
-### gene.fpkms[species=="bos taurus" & is.na(human_name),
-###               human_name := mcmapply(function(x){human.homolog(x,species="cattle")},gene_short_name)]
-### ### pan paniscus
-### gene.fpkms[species=="pan paniscus" & is.na(human_name),
-###               human_name := mcmapply(function(x){human.homolog(x,species="chimpanzee")},gene_short_name)]
 
-### reverse these because the human_name is wrong in some cases in the
-### ensembl homolog file.
 setkey(gene.fpkms,"tracking_id")
-gene.fpkms[is.na(human_name),
-              human_name := mcmapply(oma.homolog,gene_id)]
-gene.fpkms[is.na(human_name),
-           human_name := mcmapply(ensembl.homolog,gene_id)]
+gene.fpkms[is.na(human_name) & !is.na(gene_id),
+           human_name := mcmapply(identify.homolog,gene_id)]
+
+## remove all non-1:1 orthologs
+non.1.1.orthologs <-
+     gene.fpkms[,list(gene_id,n.orthologs=length(gene_id)),
+                by=list(human_name,file)][n.orthologs>1,unique(gene_id)]
+if (!is.null(non.1.1.orthologs)) {
+    gene.fpkms[non.1.1.orthologs,
+               human_name:=NA]
+}
 
 ### this shouldn't be required, but I've triggered some bug in the
 ### data.table code
@@ -119,7 +151,7 @@ combined.fpkm[name_or_id=="-" | name_or_id=="",name_or_id:=gene_id]
 combined.fpkm[human_name=="GH1" & oma_group_name=="GH2",human_name:=NA]
 
 ### diamond gene_to_human_gene.txt
-diamond.align <- fread(args[6])
+diamond.align <- fread(args[5])
 setkey(diamond.align,"gene_id")
 setnames(diamond.align,"human_gene_symbol","human_alignment_symbol")
 setkey(combined.fpkm,"gene_id")
@@ -127,7 +159,7 @@ combined.fpkm <-
     diamond.align[,list(gene_id,human_alignment_symbol)][combined.fpkm]
 
 ### gtf types
-gtf.types <- fread(args[7])
+gtf.types <- fread(args[6])
 setkey(gtf.types,"gene_id")
 setkey(combined.fpkm,"gene_id")
 combined.fpkm <-
--- data/ensembl_orthologs_long.R
+++ data/ensembl_orthologs_long.R
@@ -3,7 +3,10 @@ library(data.table)
 
 args <- commandArgs(trailingOnly=TRUE)
 
-human <- useMart("ensembl",dataset="hsapiens_gene_ensembl")
+mart <- useMart("ENSEMBL_MART_ENSEMBL",
+                dataset="hsapiens_gene_ensembl",
+                host="may2015.archive.ensembl.org")
+
 
 ensembl.orthologs.long <- list()
 
@@ -18,7 +21,7 @@ for (species in c("dnovemcinctus",
         data.table(getBM(c("ensembl_gene_id",
                            paste0(species,"_",
                                   "homolog_ensembl_gene")),
-                         values =TRUE, mart = human, bmHeader=FALSE))
+                         values =TRUE, mart = mart, bmHeader=FALSE))
     setnames(ensembl.orthologs.long[[species]],
              paste0(species,"_",
                     "homolog_ensembl_gene"),
@@ -27,12 +30,6 @@ for (species in c("dnovemcinctus",
     ensembl.orthologs.long[[species]] <-
         ensembl.orthologs.long[[species]][homolog_ensembl_gene!="",]
 }
-
-hgnc.symbols <-
-    data.table(getBM(c("ensembl_gene_id",
-                       "hgnc_symbol"),
-                     mart=human))
-
 
 ensembl.orthologs.long <- rbindlist(ensembl.orthologs.long)
 
